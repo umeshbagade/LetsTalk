8c2e9f812d5648941dabc1423090caf18f58e258
Umesh Bagade <71688971+umeshbagade@users.noreply.github.com>
2023-04-19 00:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const socket = io("https://letstalk-iemb.onrender.com")
+const socket = io("http://localhost:3000")
 const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
 
 
 // First time taking the name of the user
-const username  = prompt("Enter Your Name")
-appendMessage("You joined")
+const username  =  prompt("Enter Your Name")
+
+appendOne("You joined")
 socket.emit('user-joined', username)
 
 
 socket.on('chat-message', msg =>{
-    appendMessage(`${msg.uname} : ${msg.message}`)
+    appendMessage(msg.uname, msg.message)
 })
 
 socket.on('user-connected', name =>{
-    appendMessage(` ${name}  joined`)
+    appendOne(name +" joined")
 })
 
 socket.on('user-disconnected', name =>{
-    appendMessage(` ${name} disconnected`)
+    appendOne(name+" disconnecetd")
 })
 
 
@@ -29,15 +30,62 @@ messageForm.addEventListener('submit', e =>{
     e.preventDefault()
 
     const message = messageInput.value 
-    appendMessage(`You: ${message}`)
+    // appendMessage("You", message)
+    appendme(message)
     socket.emit('send-chat-message', message)
     messageInput.value = ""
 })
+function appendme(message) {
+    const sender = document.createElement('div')
+    sender.classList.add('sender')
+    sender.classList.add('new-align')
+    sender.innerText = "You"
 
+    const msg = document.createElement('div')
+    msg.classList.add('text')
+    msg.classList.add('sent')
+    msg.innerText = message;
 
-function appendMessage(message) {
 
     const messageElement = document.createElement('div')
-    messageElement.innerText = message
+    messageElement.classList.add('message')
+
+
+    messageElement.append(sender);
+    messageElement.append(msg)
+    messageContainer.append(messageElement)
+}
+function appendOne(msg) {
+    const sender = document.createElement('div')
+    sender.classList.add('sender')
+    sender.classList.add('atcenter')
+    sender.innerText = msg
+
+    const messageElement = document.createElement('div')
+    messageElement.classList.add('message')
+
+
+    messageElement.append(sender);
+    messageContainer.append(messageElement)
+}
+
+function appendMessage(user, message) {
+
+
+    const sender = document.createElement('div')
+    sender.classList.add('sender')
+    sender.innerText = user
+
+    const msg = document.createElement('div')
+    msg.classList.add('text')
+    msg.innerText = message;
+
+
+    const messageElement = document.createElement('div')
+    messageElement.classList.add('message')
+
+
+    messageElement.append(sender);
+    messageElement.append(msg)
     messageContainer.append(messageElement)
 }   
